7282b3d52f57e14389f103383ba99cc42bba51f6
ctokoli <matumasie@gmail.com>
2023-09-11 15:15:14 +0000

    Update teacher_class.rb

--- teacher_class.rb
+++ teacher_class.rb
@@ -1,1 +1,8 @@
+class Teacher < Person
+  attr_reader :specialization
 
+  def initialize(id, age, specialization, name = 'Unknown', parent_permission: true)
+    super(id, age, name, parent_permission)
+    @specialization = specialization
+  end
+end
