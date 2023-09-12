2eaa0c13b4bd5ebb0dd17bb93b24f8cb9736c9c5
ctokoli <matumasie@gmail.com>
2023-09-12 21:20:09 +0000

    Update person_class.rb

--- person_class.rb
+++ person_class.rb
@@ -1,8 +1,13 @@
-class Person
+require_relative 'nameable_class'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = id
     @name = name
     @age = age
@@ -20,4 +25,15 @@ class Person
   def can_use_servic
     check_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
