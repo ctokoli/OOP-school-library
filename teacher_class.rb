6372f9acf70d09f5f720057f1bb5ca269ce4ca6d
ctokoli <matumasie@gmail.com>
2023-09-16 12:05:23 +0000

    Update teacher_class.rb

--- teacher_class.rb
+++ teacher_class.rb
@@ -3,7 +3,7 @@ require_relative 'person_class'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, name = 'Unknown', specialization, parent_permission: true)
+  def initialize(age, name, specialization, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @id = Random.rand(1..1000)
     @parent_permission = parent_permission
